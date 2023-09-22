240c07f736c0b39b259d494d6a76085bdb93e273
= <=>
2023-09-22 20:21:59 +0200

    restructured

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MHardenberg/satisfaculator
+
+go 1.21.1
--- src/main.go
+++ src/main.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "github.com/MHardenberg/satisfaculator/product"
+import "satisfaculator/product"
 
 
 func main()  {
